3d50ec65c67cd6340be81dc4e5224b4fe32ee2f4
Colin Yetman <colinguitarist@gmail.com>
2025-06-01 01:57:26 -0230

    refactor update - split async functions into service.js - export and import files

--- server.js
+++ server.js
@@ -1,60 +1,10 @@
 const express = require("express");
-const axios = require("axios");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const STEAM_API_KEY = process.env.STEAM_API_KEY;
 
-// Retry util
-async function Retry(fn, retries = 3, delay = 500) {
-	for (let attempt = 1; attempt <= retries; attempt++) {
-		try {
-			return await fn();
-		} catch (e) {
-			const isLast = attempt === retries;
-			const isRetryable = e.response?.status >= 500 || e.code === "CONNECTION ABORTED";
-
-			if (isLast || !isRetryable) throw e;
-			
-			
-			console.log(`Attempt ${attempt} Failed. Retrying in ${delay}ms...`);
-			await new Promise(res => setTimeout(res, delay));
-		}
-	}
-}
-
-async function resolveVanityUrl(vanityName) {
-	return Retry(async () => {
-		const { data } = await axios.get(
-				`https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/`,
-				{
-					params: {
-						key: STEAM_API_KEY,
-						vanityurl: vanityName,
-					},
-				});
-
-				if (data.response.success !== 1) return null;
-			return data.response.steamid;
-	});
-}
-
-async function getOwnedGames(steamid) {
-	return Retry(async () => {
-		const { data } = await axios.get(
-			`https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/`,
-			{
-				params: {
-					key: STEAM_API_KEY,
-					steamid,
-					include_appinfo: true,
-					include_played_free_games: true,
-				},
-			});
-			return data.response.games || [];
-	});
-}
+const { resolveVanityUrl, getOwnedGames } = require("./service");
 
 app.get("/games", async (req, res) => {
 	const username = req.query.username;
